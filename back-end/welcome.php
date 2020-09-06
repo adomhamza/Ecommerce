15f4c102c2b3c7d741ec9eccffd1be942023e81d
adomhamza <liveghost93@gmail.com>
2020-09-06 01:18:35 +0000

    setup admin login/register pages

---
+++ back-end/welcome.php
@@ -0,0 +1,11 @@
+<?php
+include('functions.php');	
+?>
+
+
+<li><a class="sidebar-header"  href="home.php?logout='1'"><i
+                                    data-feather="log-in"></i><span>Logout</span></a>
+                    </li>
+                
+
+<h1>Welcome</h1>
